a08b9ea32bce8379ff5d8879b3c78327d3edfa61
Zhang <1319500935@qq.com>
2026-02-17 20:01:53 +0800

    2.17

---
+++ part1/quick_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+const int N = 100005;
+int q[N];
+
+void quick(int q[],int l,int r){
+    if(l >= r) return;
+    int mid = (l + r) / 2;
+    int x = q[mid] , i = l - 1 , j = r + 1;
+    while(i < j){
+        do i++; while(q[i] < x);
+        do j--; while(q[j] > x);
+        if(i < j) swap(q[i],q[j]);
+    }
+    quick(q,l,j);
+    quick(q,j + 1,r);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> q[i];
+    }
+    quick(q,0,n-1);
+    for (int i = 0; i < n; i++){
+        cout << q[i] << " ";
+    }
+    return 0;
+}
