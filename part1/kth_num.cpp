571582132257d7c1a8ab9e305189932dc65d0bca
Zhang <1319500935@qq.com>
2026-02-17 21:12:44 +0800

    2.17

--- part1/kth_num.cpp
+++ part1/kth_num.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int q[100005];
 
-void quick(int q[],int l,int r){
+void quick(int q[],int l, int r){
     if(l >= r) return;
-    int x = q[(l + r) >> 1] , i = l - 1 , j = r + 1;
+    int x = q[(l + r) >> 1], i = l - 1, j = r + 1;
     while(i < j){
         do i++; while(q[i] < x);
         do j--; while(q[j] > x);
@@ -14,15 +14,13 @@ void quick(int q[],int l,int r){
     quick(q,j + 1,r);
 }
 
-int main(){
-    int n;
-    cin >> n;
+int main (){
+    int n,k;
+    scanf("%d%d",&n,&k);
     for (int i = 0; i < n; i++){
-        cin >> q[i];
+        scanf("%d",&q[i]);
     }
     quick(q,0,n-1);
-    for (int i = 0; i < n; i++){
-        cout << q[i] << " ";
-    }
+    printf("%d",q[k-1]);
     return 0;
 }
